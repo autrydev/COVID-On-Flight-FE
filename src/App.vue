bfcddf602750b4d5ac3d730b55f160837d82c622
Stephen Smith <autrydev@gmail.com>
2020-10-24 14:47:17 -0400

    Add Material Plugin and Dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,22 @@
 <template>
-  <div id="app">
-    <md-toolbar class="md-primary">
-      <h3 class="md-title">COVID On-Flight</h3><div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    </md-toolbar>
-    <router-view/>
-  </div>
+  <v-app>
+
+    <v-app-bar app>
+      <!-- -->
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
