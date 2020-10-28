5876c2a772b3d48d0380efe2c97dfb31b0fccfa7
Stephen Smith <autrydev@gmail.com>
2020-10-28 19:55:06 -0400

    Release 1 (#5)
    * Add Login and Signup Routes
    
    * Add Material Plugin and Dependencies
    
    * Add Login Form Fields and Sign In Button
    
    * Add Dist and Node Modules Dirs
    
    * Add Styling to Login Page
    
    * Update .gitignore
    
    * Remove node_modules directory

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 import Home from '../views/Home.vue'
+import UserSignup from '../views/UserSignup.vue'
+import AdminSignup from '../views/AdminSignup.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +21,21 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/signup-u',
+    name: 'User Sign Up',
+    component: UserSignup
+  },
+  {
+    path: '/signup-a',
+    name: 'Admin Sign Up',
+    component: AdminSignup
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container class="loginpage">
+        <LoginForm />
+    </v-container>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue'
+
+export default {
+  name: 'Login',
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style scoped>
+.loginpage {
+    /** background-color: green; */
+    color: black;
+}
+</style>
