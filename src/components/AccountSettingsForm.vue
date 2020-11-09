d50ef47531a105679445827258062c95cf7f2613
rcn66 <rcorreanetto@ufl.edu>
2020-11-09 02:00:48 -0500

    Created User Account Settings page
    Set up framework for user account settings page with basic validation

--- src/components/AccountSettingsForm.vue
+++ src/components/AccountSettingsForm.vue
@@ -1,102 +1,156 @@
 <template>
-  <v-top-navigation
-    :value="value"
-    color="primary"
-  >
-    <v-container fluid>
-        <v-flex xs12 class="text-xl-center" mt-5>
-            <v-btn to="/dashboard"  x-large outlined> Dashboard<v-icon>mdi-history</v-icon></v-btn>
-            <v-btn to="/covid19informaion" x-large outlined>COVID-19 Information<v-icon>mdi-heart</v-icon></v-btn>
-            <v-btn to="/mycovidstatus" x-large outlined>My Covid Status<v-icon>mdi-alert</v-icon></v-btn>
-            <v-btn class="white--text" color="#2962FF" elevation="5" to="/accountsettings" x-large >Account Settings<v-icon>mdi-account-edit</v-icon></v-btn>
-        </v-flex>
+  <div id="container">
+    <v-container id="account-settings">
+      <v-form v-model="valid" :disabled="!responsive" ref="form">
+        <v-row>
+          <v-col cols="4">
+            <v-subheader :dark="true" class="prefix">First Name</v-subheader>
+          </v-col>
+          <v-col cols="6" class="text-box">
+            <v-text-field
+              v-model="firstName"
+              :dark="true"
+              placeholder="FETCH_FIRST_NAME"
+              outlined
+              :required="responsive"
+              :rules="[firstNameRules.required]"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4">
+            <v-subheader :dark="true" class="prefix">Last Name</v-subheader>
+          </v-col>
+          <v-col cols="6" class="text-box">
+            <v-text-field
+              v-model="lastName"
+              :dark="true"
+              placeholder="FETCH_LAST_NAME"
+              outlined
+              :required="responsive"
+              :rules="[lastNameRules.required]"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4">
+            <v-subheader :dark="true" class="prefix">Email Address</v-subheader>
+          </v-col>
+          <v-col cols="6" class="text-box">
+            <v-text-field
+              v-model="email"
+              :dark="true"
+              placeholder="FETCH_EMAIL"
+              outlined
+              :required="responsive"
+              :rules="[emailRules.required, emailRules.valid]"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4">
+            <v-subheader :dark="true" class="prefix">Phone Number</v-subheader>
+          </v-col>
+          <v-col cols="6" class="text-box">
+            <v-text-field
+              v-model="phoneNumber"
+              :dark="true"
+              placeholder="FETCH_NUMBER"
+              outlined
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row v-show="!responsive">
+          <v-col>
+            <v-btn :dark="true" id="change-settings" @click="openForm()"
+              >Update Settings</v-btn
+            >
+          </v-col>
+        </v-row>
+        <v-row v-show="responsive">
+          <v-col>
+            <v-btn :dark="true" id="update-settings" @click="updateSettings()"
+              >Save changes</v-btn
+            >
+          </v-col>
+          <v-col>
+            <v-btn :dark="true" id="cancel-settings" @click="cancelChanges()"
+              >Cancel changes</v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-form>
     </v-container>
-
-
-
-  </v-top-navigation>
+  </div>
 </template>
+
 <script>
-
-  export default {
-
-    data: () => ({
-        valid: false,
-        show1: false,
-        show2: false,
-        firstName: '',
-        firstNameRules: {
-            required: v => !!v || 'First namasdsade is required', /*If value is falsey (empty string), then output message*/
-        },
-        lastName: '',
-        lastNameRules: {
-            required: v => !!v || 'Last name is required',
-        },
-        email: '',
-        emailRules: {
-            required: v => !!v || 'Email address is required',
-            valid: v => /.+@.+/.test(v) || 'Email address must be valid',
-        },
-        password1: '',
-        pass1Rules: {
-            required: v => !!v || 'Password is required',
-        },
-        password2: '',
-        pass2Rules: {
-            required: v => !!v || 'Retyping your password is required',
-        }
-        }),
-    computed: {
-        checkPasswords() {
-            return this.password1 === this.password2 || 'Passwords must match';
-        },
-    }
-  };
+export default {
+  data: () => ({
+    responsive: false,
+    valid: false,
+    show1: false,
+    show2: false,
+    firstName: "",
+    firstNameRules: {
+      required: (v) =>
+        !!v ||
+        "First name is required" /*If value is falsey (empty string), then output message*/,
+    },
+    lastName: "",
+    lastNameRules: {
+      required: (v) => !!v || "Last name is required",
+    },
+    email: "",
+    emailRules: {
+      required: (v) => !!v || "Email address is required",
+      valid: (v) => /.+@.+/.test(v) || "Email address must be valid",
+    },
+    phoneNumber: "",
+  }),
+  methods: {
+    openForm() {
+      this.responsive = true;
+    },
+    updateSettings() {
+      if (this.$refs.form.validate() === true) {
+        this.responsive = false;
+        //submit changes
+      }
+    },
+    cancelChanges() {
+      this.responsive = false;
+      this.$refs.form.reset();
+    },
+    checkPasswords() {
+      return this.password1 === this.password2 || "Passwords must match";
+    },
+  },
+};
 </script>
 
 <style scoped>
-#container {
+#account-settings {
+  margin-top: 5%;
+  background-color: #1c1e1f;
+  width: min(50%, 500px);
 }
-#signup-card {
-    width: min(50%, 500px);
-    background-color: #FFFFFF;
+#change-settings {
+  width: 100%;
+  height: 4em;
+  margin-bottom: 1em;
+  background-color: rgb(7, 61, 161);
 }
-#signup-form {
-    margin-top: 5%;
-    background-color: #FFFFFF;
-    padding: 2em;
+#update-settings {
+  width: 100%;
+  height: 4em;
+  margin-bottom: 1em;
+  background-color: #006600;
 }
-h1 {
-    text-align: center;
-    padding-bottom: 1em;
-    color: rgb(219, 214, 214);
-}
-#names {
-    padding-bottom: 0;
-    margin-bottom: 0;
-    height: 100px;
-}
-#first-name {
-    padding-left: 0;
-}
-#last-name {
-    padding-right: 0;
-    padding-bottom: 0;
-}
-#btn-signup {
-    width: 100%;
-    height: 4em;
-    margin-bottom: 1em;
-    background-color: rgb(255, 136, 0);
-}
-p {
-    padding-top: 5em;
-    color: rgb(219, 214, 214);
-}
-#copyright {
-    text-align: center;
-}
-.link {
-    text-decoration: none;
+#cancel-settings {
+  width: 100%;
+  height: 4em;
+  margin-bottom: 1em;
+  background-color: rgb(141, 16, 16);
 }
 </style>
--- src/views/AccountSettings.vue
+++ src/views/AccountSettings.vue
@@ -1,23 +1,52 @@
 <template>
-    <v-container class="accountsettingspage">
-        <AccountSettingsForm />
-    </v-container>
+  <v-container class="accountsettingspage">
+    <v-top-navigation :value="value" color="primary">
+      <v-container fluid>
+        <v-flex xs12 class="text-xl-center" mt-5>
+          <v-btn to="/dashboard" x-large outlined>
+            Dashboard<v-icon>mdi-history</v-icon></v-btn
+          >
+          <v-btn to="/covid19informaion" x-large outlined
+            >COVID-19 Information<v-icon>mdi-heart</v-icon></v-btn
+          >
+          <v-btn to="/mycovidstatus" x-large outlined
+            >My Covid Status<v-icon>mdi-alert</v-icon></v-btn
+          >
+          <v-btn
+            to="/accountsettings"
+            class="white--text"
+            color="#2962FF"
+            elevation="5"
+            x-large
+            >Account Settings<v-icon>mdi-account-edit</v-icon></v-btn
+          >
+        </v-flex>
+      </v-container>
+    </v-top-navigation>
+    <!--<DashboardHeader></DashboardHeader>-->
+    <AccountSettingsForm />
+    <app-footer></app-footer>
+  </v-container>
 </template>
 
 <script>
-import AccountSettingsForm from '@/components/AccountSettingsForm.vue'
+import AccountSettingsForm from "@/components/AccountSettingsForm.vue";
+import AppFooter from "@/components/AppFooter.vue";
+//import DashboardHeader from "@/components/DashboardHeader";
 
 export default {
-  name: 'AccountSettings',
+  name: "AccountSettings",
   components: {
-    AccountSettingsForm
-  }
-}
+    AccountSettingsForm,
+    AppFooter,
+    //DashboardHeader,
+  },
+};
 </script>
 
 <style scoped>
-.dashboardpage {
-    /** background-color: green; */
-    color: white;
+.accountsettingspage {
+  /** background-color: green; */
+  background-color: #1c1e1f;
 }
 </style>
