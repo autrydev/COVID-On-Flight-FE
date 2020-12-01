7e7e50b4f05f2b05ba361b93c5b2c85c2c90f819
rcn66 <rcorreanetto@ufl.edu>
2020-12-01 17:23:22 -0500

    copying code

--- src/components/SurveyForm.vue
+++ src/components/SurveyForm.vue
@@ -1,79 +1,277 @@
 <template>
     <v-container>
         <v-form v-model="valid" ref="form">
-        <v-row>
-            <v-col cols="4">
-            <v-subheader :dark="false" class="prefix">First Name</v-subheader>
-            </v-col>
-            <v-col cols="6" class="text-box">
-            <v-text-field
-                v-model="firstName"
-                :dark="false"
-                outlined
-                :required="responsive"
-                :rules="[firstNameRules.required]"
-            ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col cols="4">
-            <v-subheader :dark="false" class="prefix">Last Name</v-subheader>
-            </v-col>
-            <v-col cols="6" class="text-box">
-            <v-text-field
-                v-model="lastName"
-                :dark="false"
-                outlined
-                :required="responsive"
-                :rules="[lastNameRules.required]"
-            ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col cols="4">
-            <v-subheader :dark="false" class="prefix">Email Address</v-subheader>
-            </v-col>
-            <v-col cols="6" class="text-box">
-            <v-text-field
-                v-model="email"
-                :dark="false"
-                outlined
-                :required="responsive"
-                :rules="[emailRules.required, emailRules.valid]"
-            ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col cols="4">
-            <v-subheader :dark="false" class="prefix">Phone Number</v-subheader>
-            </v-col>
-            <v-col cols="6" class="text-box">
-            <v-text-field
-                v-model="phoneNumber"
-                :dark="false"
-                outlined
-            ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row v-show="!responsive">
-            <v-col>
-            <v-btn :dark="true" id="change-settings" @click="openForm()"
-                >Update Settings</v-btn
-            >
-            </v-col>
-        </v-row>
-        <v-row v-show="responsive">
-            <v-col>
-            <v-btn :dark="true" id="update-settings" @click="updateAccountInfo()"
-                >Save changes</v-btn
-            >
-            </v-col>
-            <v-col>
-            <v-btn :dark="true" id="cancel-settings" @click="cancelChanges()"
-                >Cancel changes</v-btn
-            >
-            </v-col>
-        </v-row>
+            <v-card class="surveyquestions">
+                <v-data-table
+                    :items="flights"
+                    class="elevation-2"
+                >
+                    <template v-slot:top>
+                    <v-toolbar
+                        flat
+                    >
+                        <v-toolbar-title>Flights</v-toolbar-title>
+                        <v-spacer></v-spacer>
+                        <v-dialog
+                        v-model="dialog"
+                        max-width="500px"
+                        >
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-btn
+                            color="primary"
+                            dark
+                            class="mb-2"
+                            v-bind="attrs"
+                            v-on="on"
+                            >
+                            Search for Flights
+                            </v-btn>
+                        </template>
+                        <v-card>
+                            <v-card-title>
+                            <span class="headline">Search by flight information</span>
+                            </v-card-title>
+
+                            <v-card-text>
+                            <v-container>
+                                <v-row>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                        md="12"
+                                    >
+                                        <v-text-field
+                                        v-model="searchParameters.flight_id"
+                                        label="Flight Number"
+                                        clearable
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                        md="6"
+                                    >
+                                        <v-menu
+                                            ref="menu1"
+                                            v-model="menu1"
+                                            :close-on-content-click="false"
+                                            :return-value.sync="searchParameters.from_date"
+                                            transition="scale-transition"
+                                            offset-y
+                                            min-width="290px"
+                                          >
+                                            <template v-slot:activator="{ on, attrs }">
+                                              <v-text-field
+                                                v-model="searchParameters.from_date"
+                                                label="Departure Date"
+                                                prepend-icon="mdi-calendar"
+                                                readonly
+                                                v-bind="attrs"
+                                                v-on="on"
+                                                clearable
+                                              ></v-text-field>
+                                            </template>
+                                            <v-date-picker
+                                              v-model="searchParameters.from_date"
+                                              no-title
+                                              scrollable
+                                            >
+                                              <v-spacer></v-spacer>
+                                              <v-btn
+                                                text
+                                                color="primary"
+                                                @click="menu = false"
+                                              >
+                                                Cancel
+                                              </v-btn>
+                                              <v-btn
+                                                text
+                                                color="primary"
+                                                @click="$refs.menu1.save(searchParameters.from_date)"
+                                              >
+                                                OK
+                                              </v-btn>
+                                            </v-date-picker>
+                                        </v-menu>
+                                    </v-col>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                        md="6"
+                                    >
+                                        <v-menu
+                                            ref="menu2"
+                                            v-model="menu2"
+                                            :close-on-content-click="false"
+                                            :return-value.sync="searchParameters.to_date"
+                                            transition="scale-transition"
+                                            offset-y
+                                            min-width="290px"
+                                          >
+                                            <template v-slot:activator="{ on, attrs }">
+                                              <v-text-field
+                                                v-model="searchParameters.to_date"
+                                                label="Arrival Date"
+                                                prepend-icon="mdi-calendar"
+                                                readonly
+                                                v-bind="attrs"
+                                                v-on="on"
+                                                clearable
+                                              ></v-text-field>
+                                            </template>
+                                            <v-date-picker
+                                              v-model="searchParameters.to_date"
+                                              no-title
+                                              scrollable
+                                            >
+                                              <v-spacer></v-spacer>
+                                              <v-btn
+                                                text
+                                                color="primary"
+                                                @click="menu = false"
+                                              >
+                                                Cancel
+                                              </v-btn>
+                                              <v-btn
+                                                text
+                                                color="primary"
+                                                @click="$refs.menu2.save(searchParameters.to_date)"
+                                              >
+                                                OK
+                                              </v-btn>
+                                            </v-date-picker>
+                                        </v-menu>
+                                    </v-col>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                        md="6"
+                                    >
+                                        <v-text-field
+                                        v-model="searchParameters.departure_city"
+                                        label="Departure City"
+                                        clearable
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                        md="6"
+                                    >
+                                        <v-text-field
+                                        v-model="searchParameters.arrival_city"
+                                        label="Arrival City"
+                                        clearable
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                        md="12"
+                                    >
+                                        <v-card
+                                            class="mx-auto"
+                                            max-width="300"
+                                            tile
+                                          >
+                                            <v-list dense>
+                                              <v-subheader>COVID Status</v-subheader>
+                                              <v-list-item-group
+                                                v-model="covidOption"
+                                                color="primary"
+                                                mandatory
+                                              >
+                                                <v-list-item
+                                                  v-for="(option, i) in covidOptions"
+                                                  :key="i"
+                                                >
+                                                  <v-list-item-icon>
+                                                    <v-icon v-text="option.icon"></v-icon>
+                                                  </v-list-item-icon>
+                                                  <v-list-item-content>
+                                                    <v-list-item-title v-text="option.text"></v-list-item-title>
+                                                  </v-list-item-content>
+                                                </v-list-item>
+                                              </v-list-item-group>
+                                            </v-list>
+                                          </v-card>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                            </v-card-text>
+
+                            <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                color="blue darken-1"
+                                text
+                                @click="close"
+                            >
+                                Cancel
+                            </v-btn>
+                            <v-btn
+                                color="blue darken-1"
+                                text
+                                @click="searchFlights()"
+                                v-on:click="close"
+                            >
+                                Search
+                            </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                        </v-dialog>
+                        <v-dialog v-model="dialogDelete" max-width="500px">
+                        <v-card>
+                            <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+                            <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                            <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                            <v-spacer></v-spacer>
+                            </v-card-actions>
+                        </v-card>
+                        </v-dialog>
+                    </v-toolbar>
+                    </template>
+                    <template v-slot:item.actions="{ item }">
+                    <v-icon
+                        small
+                        class="mr-2"
+                        @click="editItem(item)"
+                    >
+                        mdi-pencil
+                    </v-icon>
+                    <v-icon
+                        small
+                        @click="deleteItem(item)"
+                    >
+                        mdi-delete
+                    </v-icon>
+                    </template>
+                    <template v-slot:no-data>
+                        Search for flights!
+                    </template>
+                </v-data-table>
+            </v-card>
+            <v-row v-show="!responsive">
+                <v-col>
+                <v-btn :dark="true" id="change-settings" @click="openForm()"
+                    >Update Settings</v-btn
+                >
+                </v-col>
+            </v-row>
+            <v-row v-show="responsive">
+                <v-col>
+                <v-btn :dark="true" id="update-settings" @click="updateAccountInfo()"
+                    >Save changes</v-btn
+                >
+                </v-col>
+                <v-col>
+                <v-btn :dark="true" id="cancel-settings" @click="cancelChanges()"
+                    >Cancel changes</v-btn
+                >
+                </v-col>
+            </v-row>
         </v-form>
     </v-container>
 </template>
@@ -83,24 +281,20 @@ import axios from 'axios'
 
 export default {
   data: () => ({
-    responsive: false,
     valid: false,
-    firstName: "",
-    firstNameRules: {
-      required: (v) =>
-        !!v ||
-        "First name is required" /*If value is falsey (empty string), then output message*/,
-    },
-    lastName: "",
-    lastNameRules: {
-      required: (v) => !!v || "Last name is required",
-    },
-    email: "",
-    emailRules: {
-      required: (v) => !!v || "Email address is required",
-      valid: (v) => /.+@.+/.test(v) || "Email address must be valid",
-    },
-    phoneNumber: "",
+    symptoms: [
+        "Fever or chills",
+        "Cough",
+        "Shortness of breath or difficulty breathing",
+        "Fatigue",
+        "Muscle or body aches",
+        "Headache",
+        "Loss of taste or smell",
+        "Sore throat",
+        "Congestion or runny nose",
+        "Nausea or vomiting",
+        "Diarrhea",
+    ],
   }),
   methods: {
     openForm() {
