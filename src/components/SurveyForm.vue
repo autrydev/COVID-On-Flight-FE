8a76df999e8ab51b08f37b93cdb4172b362b2e41
rcn66 <rcorreanetto@ufl.edu>
2020-12-01 18:21:39 -0500

    Created survey questions and radio buttons
    Not fully functional

--- src/components/SurveyForm.vue
+++ src/components/SurveyForm.vue
@@ -1,46 +1,65 @@
 <template>
-    <v-container>
-        <v-card class="flighttables">
-            <v-data-table
-                :items="symptoms"
-                class="elevation-2"
+    <v-container id="survey-component">
+      <v-form v-model="valid" ref="form">
+        <ul id="survey-questions"> 
+            <li v-for="(symptom, i) in symptoms" :key="i">
+                <v-row>
+                    <v-col
+                    >
+                        {{ symptom.text }}
+                    </v-col>
+                        <v-radio-group
+                        v-model="row"
+                        row
+                        mandatory
+                        >
+                        <v-radio
+                            label="Yes"
+                            value=""
+                        ></v-radio>
+                        <v-radio
+                            label="No"
+                            value=""
+                        ></v-radio>
+                        </v-radio-group>
+                </v-row>
+            </li>
+        </ul>
+        <v-row>
+          <v-col>
+            <v-btn :dark="true" id="update-symptoms" @click="updateCovidStatus()"
+              >Update Symptoms</v-btn
             >
-            </v-data-table>
-        </v-card>
+          </v-col>
+        </v-row>
+      </v-form>
     </v-container>
 </template>
 
 <script>
-import axios from 'axios'
+//import axios from 'axios'
 
 export default {
   data: () => ({
     valid: false,
     symptoms: [
-        "Fever or chills",
-        "Cough",
-        "Shortness of breath or difficulty breathing",
-        "Fatigue",
-        "Muscle or body aches",
-        "Headache",
-        "Loss of taste or smell",
-        "Sore throat",
-        "Congestion or runny nose",
-        "Nausea or vomiting",
-        "Diarrhea",
+        { text: "Fever or chills", value: null },
+        { text: "Cough", value: null },
+        { text: "Shortness of breath or difficulty breathing", value: null },
+        { text: "Fatigue", value: null },
+        { text: "Muscle or body aches", value: null },
+        { text: "Headache", value: null },
+        { text: "Loss of taste or smell", value: null },
+        { text: "Sore throat", value: null },
+        { text: "Congestion or runny nose", value: null },
+        { text: "Nausea or vomiting", value: null },
+        { text: "Diarrhea", value: null },
     ],
   }),
   methods: {
-    openForm() {
-      this.responsive = true;
-    },
-    cancelChanges() {
-      this.responsive = false;
-      this.fetchAccountInfo();
-    },
     updateCovidStatus() {
-        if (this.$refs.form.validate() === true) {
-            axios.post('/accountsettings', {
+        /*if (this.$refs.form.validate() === true) {
+            axios.post('/updatecovidstatus', {
                 id: localStorage.user,
                 first_name: this.firstName,
                 last_name: this.lastName,
@@ -57,14 +76,26 @@ export default {
             .catch(e => {
                 console.log(e)
             })
-        }
+        }*/
     }
-  },
-  beforeMount() {
-    this.fetchAccountInfo()
   },
 };
 </script>
 
 <style scoped>
+    #survey-card {
+    }
+    #survey-questions {
+        list-style-type: none;
+    }
+    #survey-component {
+        margin-top: 5%;
+        width: min(50%, 500px);
+    }
+    #update-symptoms {
+        width: 100%;
+        height: 4em;
+        margin-bottom: 1em;
+        background-color: #006600;
+    }
 </style>
